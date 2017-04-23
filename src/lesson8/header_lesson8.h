267942adaf8a22c05ab559bddd99d28cb4bbde2d
Aziz Hararwala <ahararwala@pillartechnology.com>
2017-04-23 10:41:31 -0400

    Initial commit for lesson 8

---
+++ src/lesson8/header_lesson8.h
@@ -0,0 +1,14 @@
+#ifndef LESSON8_HEADER_LESSON8_H
+#define LESSON8_HEADER_LESSON8_H
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <cstddef>
+#include<iomanip>
+
+using namespace std;
+
+#endif //LESSON8_HEADER_LESSON8_H
---
+++ src/lesson8/main.cpp
@@ -0,0 +1,11 @@
+#include "header_lesson8.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello world, I am ready for C++ Lesson 7: Learning Arrays\n";
+//    printMessage();
+//    try_increment();
+//    passingArrayExamples();
+    searchInArray();
+}
